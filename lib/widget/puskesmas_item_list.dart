0857335133922dbcc406b5d47ad3fa2acdcf1d53
roningrum <rizkioktavianingrum@gmail.com>
2021-04-24 13:58:28 +0700

    Puskesmas List done. kurang jarak

--- lib/widget/puskesmas_item_list.dart
+++ lib/widget/puskesmas_item_list.dart
@@ -1,6 +1,7 @@
-import 'package:flutter/cupertino.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:pustaka_app/const.dart';
 
 class PuskesmasItemList extends StatelessWidget {
@@ -14,56 +15,63 @@ class PuskesmasItemList extends StatelessWidget {
         onTap: () {},
         child: Card(
           elevation: 0,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
+          child: Container(
+            margin: EdgeInsets.all(10),
             child: Row(
               children: <Widget>[
                 Container(
                   width: 97,
                   height: 84,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: NetworkImage(foto), fit: BoxFit.cover
+                  child: CachedNetworkImage(
+                    imageUrl: foto,
+                    imageBuilder: (context, imageProvider) => Container(
+                      decoration: BoxDecoration(
+                          color: Colors.black26,
+                          image: DecorationImage(
+                              image: imageProvider, fit: BoxFit.cover),
+                          borderRadius: BorderRadius.circular(8)),
                     ),
-                    borderRadius: BorderRadius.circular(6),
                   ),
                 ),
                 SizedBox(width: 12),
                 Expanded(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            nama,
-                            style: kPustakaBlackBoldMedium.copyWith(
-                                fontSize: 13, fontWeight: FontWeight.bold),
-                          ),
-                          SizedBox(width: 20),
-                          Text(
-                            alamat,
-                            style: kPustakaBlackRegular.copyWith(
-                                fontSize: 12, fontWeight: FontWeight.w600),
-                          ),
-                          SizedBox(width: 17),
-                          Align(
-                            alignment: Alignment.bottomRight,
-                            child: RichText(
-                              text: TextSpan(children: [
-                                WidgetSpan(
-                                    child: Icon(
-                                      Icons.location_on,
-                                      color: kPrimaryColor,
-                                    )),
-                                TextSpan(
-                                    text: "1.2km",
-                                    style: kPustakaGreenMedium.copyWith(fontSize: 13))
-                              ]),
-                            ),
-                          )
-                        ],
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        nama,
+                        style: kPustakaBlackBoldMedium.copyWith(
+                            fontSize: 13, fontWeight: FontWeight.bold),
                       ),
-                    ),
+                      Container(
+                        margin: EdgeInsets.only(right: 12, top: 10),
+                        child: Text(
+                          alamat,
+                          style: kPustakaBlackRegular.copyWith(
+                              fontSize: 12, fontWeight: FontWeight.w100),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(right: 24, top: 15),
+                        child: Align(
+                          alignment: Alignment.bottomRight,
+                          child: RichText(
+                            text: TextSpan(children: [
+                              WidgetSpan(
+                                  child: SvgPicture.asset(
+                                      'assets/menu/place.svg')),
+                              TextSpan(
+                                  text: "1.2km",
+                                  style: kPustakaGreenMedium.copyWith(
+                                      fontSize: 13))
+                            ]),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
               ],
             ),
           ),
