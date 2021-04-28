2a16ee44abec5d86b42cbc45d2cae0a29d6149de
roningrum <rizkioktavianingrum@gmail.com>
2021-04-28 12:43:33 +0700

    stuck search textfield

--- lib/screens/puskesmas_search.dart
+++ lib/screens/puskesmas_search.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:toast/toast.dart';
 
 import '../const.dart';
 
@@ -13,40 +14,62 @@ class _PuskesmasSearchState extends State<PuskesmasSearch> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: kSurfaceColor,
-        leading: IconButton(
-          icon: Icon(
-            Icons.arrow_back_ios_rounded,
-            color: kFontColor,
-          ),
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-        ),
-        title: Container(
-          width: 290,
-          height: 35,
-          child: TextField(
-            decoration: InputDecoration(
-              hintStyle: kPustakaGreenRegular.copyWith(fontSize: 15),
-              hintText: "Cari Puskesmas",
-              border: OutlineInputBorder(
-                // width: 0.0 produces a thin "hairline" border
-                borderRadius: BorderRadius.all(Radius.circular(90.0)),
-                  borderSide: BorderSide(
-                    color: Colors.grey[200],
-                  )
-                //borderSide: const BorderSide(),
+        appBar: AppBar(
+            backgroundColor: kSurfaceColor,
+            leading: IconButton(
+              icon: Icon(
+                Icons.arrow_back_ios_rounded,
+                color: kFontColor,
               ),
-              filled: true,
-              fillColor: Colors.white24,
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
             ),
-
-          ),
-
-        ),
-      ),
+            title: Container(
+                width: 290,
+                height: 55,
+                padding: EdgeInsets.symmetric(vertical: 4),
+                child: TextField(
+                    autocorrect: true,
+                    decoration: InputDecoration(
+                      hintText: 'Type Text Here...',
+                      contentPadding: EdgeInsets.all(10.0),
+                      hintStyle: kPustakaBlackBoldMedium.copyWith(fontSize: 15, color: Color(0xFF4D4D4D)),
+                      filled: true,
+                      fillColor: Color(0xFFE9E8E8),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(4)),
+                        borderSide: BorderSide(color: Colors.transparent, width: 1),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(4)),
+                        borderSide: BorderSide(color: Colors.transparent, width: 1),
+                      ),
+                    ),
+                  onChanged: onSearchTextChanged
+                  ,
+                )
+            )
+        )
     );
+  }
+
+  onSearchTextChanged(String text) async{
+    if(text.isEmpty){
+      setState(() {
+        Toast.show('Ini kosong', context, duration: Toast.LENGTH_SHORT, gravity: Toast.BOTTOM);
+      //   Fluttertoast.showToast(
+      //       msg: 'This is toast notification',
+      //       toastLength: Toast.LENGTH_SHORT,
+      //       gravity: ToastGravity.BOTTOM,
+      //       timeInSecForIos: 1,
+      //       backgroundColor: Colors.red,
+      //       textColor: Colors.yellow
+      //   );
+        return;
+      });
+    } else{
+      Toast.show('$text', context, duration: Toast.LENGTH_SHORT, gravity: Toast.BOTTOM);
+    }
   }
 }
