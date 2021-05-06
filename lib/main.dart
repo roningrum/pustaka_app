348149ebe71f235ed24d2b1821a80b2cbd3e7132
okta <rizkioktavianingrum@gmail.com>
2021-05-06 10:50:49 +0700

    search done

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,6 @@ import 'package:pustaka_app/screens/puskesmas_search.dart';
 void main() {
   runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
--- lib/screens/puskesmas_search.dart
+++ lib/screens/puskesmas_search.dart
@@ -1,5 +1,11 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
+import 'package:pustaka_app/data/puskesmas.dart';
+import 'package:pustaka_app/helper/puskesmas_list.dart';
+import 'package:pustaka_app/screens/puskesmas_detail_page.dart';
+import 'package:pustaka_app/widget/puskesmas_item_list.dart';
+
 import '../const.dart';
 
 class PuskesmasSearch extends StatefulWidget {
@@ -10,19 +16,31 @@ class PuskesmasSearch extends StatefulWidget {
 }
 
 class _PuskesmasSearchState extends State<PuskesmasSearch> {
+  bool _loading;
+  List<Puskesmas> sarkesList = [];
+  List<Puskesmas> _searchResult = [];
+
+
   TextEditingController searchController = TextEditingController();
 
-  filterPuskesmas() {
-    print('current text ${searchController.text}');
+  void getPuskesmas() async {
+    PuskesmasList puskesmasItem = PuskesmasList();
+    await puskesmasItem.getPuskesmas();
+    sarkesList = puskesmasItem.puskesmasList;
+    setState(() {
+      if (sarkesList.length != 0) {
+        print("Data ada");
+      } else {
+        print("Data tidak ada");
+      }
+    });
   }
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    searchController.addListener(() {
-      filterPuskesmas();
-    });
+    getPuskesmas();
   }
 
   @override
@@ -50,23 +68,85 @@ class _PuskesmasSearchState extends State<PuskesmasSearch> {
                     hintText: 'Type Text Here...',
                     contentPadding: EdgeInsets.all(10.0),
                     hintStyle: kPustakaBlackBoldMedium.copyWith(
-                        fontSize: 15, color: Color(0xFF4D4D4D).withOpacity(0.5)),
+                        fontSize: 15,
+                        color: Color(0xFF4D4D4D).withOpacity(0.5)),
                     filled: true,
                     fillColor: Color(0xFFE9E8E8),
                     enabledBorder: OutlineInputBorder(
                       borderRadius: BorderRadius.all(Radius.circular(4)),
                       borderSide:
-                          BorderSide(color: Colors.transparent, width: 1),
+                      BorderSide(color: Colors.transparent, width: 1),
                     ),
                     focusedBorder: OutlineInputBorder(
                       borderRadius: BorderRadius.all(Radius.circular(4)),
                       borderSide:
-                          BorderSide(color: Colors.transparent, width: 1),
+                      BorderSide(color: Colors.transparent, width: 1),
                     ),
                   ),
                   onChanged: (txt) {
-                    print("$txt");
+                   _searchResult.clear();
+                   if(txt.isEmpty){
+                     setState(() {
+                     });
+                     return;
+                   }
+                   sarkesList.forEach((puskesmas) {
+                     if(puskesmas.nama.toLowerCase().contains(txt) || puskesmas.nama.contains(txt)){
+                       _searchResult.add(puskesmas);
+                     }
+                   });
+                   setState(() {});
                   },
-                ))));
+                ))),
+        body: Column(
+          children : <Widget>[
+          Expanded(
+              child: _searchResult.length !=0 || searchController.text.isNotEmpty ?ListView.builder(
+                  itemCount: _searchResult.length,
+                  shrinkWrap: true,
+                  physics: ClampingScrollPhysics(),
+                  itemBuilder: (context, index) {
+                    return InkWell(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => PuskesmasDetail(
+                                  puskesmas: _searchResult[index],
+                                )));
+                        print('data ${_searchResult[index]}');
+                      },
+                      child: PuskesmasItemList(
+                        foto: _searchResult[index].foto ?? "",
+                        nama: _searchResult[index].nama ?? "",
+                        alamat: _searchResult[index].alamat ?? "",
+                      ),
+                    );
+                  }):
+              ListView.builder(
+                  itemCount: sarkesList.length,
+                  shrinkWrap: true,
+                  physics: ClampingScrollPhysics(),
+                  itemBuilder: (context, index) {
+                    return InkWell(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => PuskesmasDetail(
+                                  puskesmas: sarkesList[index],
+                                )));
+                        print('data ${sarkesList[index]}');
+                      },
+                      child: PuskesmasItemList(
+                        foto: sarkesList[index].foto ?? "",
+                        nama: sarkesList[index].nama ?? "",
+                        alamat: sarkesList[index].alamat ?? "",
+                      ),
+                    );
+                  })
+
+          ),
+        ]));
   }
 }
