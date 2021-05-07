1aa2697668c703ca6698b3462e1dbd8365e9e4c9
okta <rizkioktavianingrum@gmail.com>
2021-05-07 11:48:39 +0700

    stiill stuck mapping object to widget

---
+++ lib/screens/puskesmas_daftar_data_identitas.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DaftarDataIdentitas extends StatefulWidget {
+  const DaftarDataIdentitas({Key key}) : super(key: key);
+
+  @override
+  _DaftarDataIdentitasState createState() => _DaftarDataIdentitasState();
+}
+
+class _DaftarDataIdentitasState extends State<DaftarDataIdentitas> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/screens/puskesmas_lihat_antrian_detail.dart
+++ lib/screens/puskesmas_lihat_antrian_detail.dart
@@ -1,14 +1,39 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:pustaka_app/data/puskesmas.dart';
 import 'package:pustaka_app/screens/puskesmas_lihat_antrian.dart';
 import 'package:pustaka_app/widget/puskesmas_antrian_item.dart';
-
+import 'package:http/http.dart' as http;
 import '../const.dart';
 
-class PuskesmasAntrianDetail extends StatelessWidget {
+class PuskesmasAntrianDetail extends StatefulWidget {
   final Puskesmas puskesmas;
-
   const PuskesmasAntrianDetail({Key key, this.puskesmas}) : super(key: key);
+
+  @override
+  _PuskesmasAntrianDetailState createState() => _PuskesmasAntrianDetailState();
+}
+
+class _PuskesmasAntrianDetailState extends State<PuskesmasAntrianDetail> {
+
+  Future<void> getAntrian() async{
+    var url = Uri.parse("${widget.puskesmas.antrian}");
+    var response = await http.get(url);
+    Map jsonData = jsonDecode(response.body);
+    print(jsonData);
+  }
+
+  void getAntrianPuskesmas(){
+    getAntrian();
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getAntrianPuskesmas();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,7 +57,7 @@ class PuskesmasAntrianDetail extends StatelessWidget {
           Container(
             margin: EdgeInsets.only(top: 16),
             child: Center(
-              child: Text("Status Antrian Puskesmas ${puskesmas.nama}",
+              child: Text("Status Antrian Puskesmas ${widget.puskesmas.nama}",
                   style: kPustakaBlackRegular.copyWith(fontSize: 13)),
             ),
           ),
