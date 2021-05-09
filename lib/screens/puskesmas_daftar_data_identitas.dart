fe67f8d3163e402d27c7a6244d512fdcd1702345
roningrum <rizkioktavianingrum@gmail.com>
2021-05-09 13:40:49 +0700

    create form

--- lib/screens/puskesmas_daftar_data_identitas.dart
+++ lib/screens/puskesmas_daftar_data_identitas.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:pustaka_app/const.dart';
+import 'package:pustaka_app/data/puskesmas.dart';
 
 class DaftarDataIdentitas extends StatefulWidget {
-  const DaftarDataIdentitas({Key key}) : super(key: key);
+  final Puskesmas puskesmas;
+  const DaftarDataIdentitas({Key key, this.puskesmas}) : super(key: key);
 
   @override
   _DaftarDataIdentitasState createState() => _DaftarDataIdentitasState();
@@ -10,6 +13,91 @@ class DaftarDataIdentitas extends StatefulWidget {
 class _DaftarDataIdentitasState extends State<DaftarDataIdentitas> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back_ios_rounded,
+              color: kSurfaceColor,
+            ),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+          title: Text('Data Identitas', style: kPustakaGreenMedium.copyWith(fontSize: 19)),
+        ),
+        body: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.only(top: 8),
+              child: Text('Pendaftaran Online ${widget.puskesmas.nama}', style: kPustakaBlackRegular.copyWith(
+                fontSize: 13), textAlign: TextAlign.center,),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 2),
+              child: Text('Pendaftaran Online ${widget.puskesmas.nama}', style: kPustakaBlackRegular.copyWith(
+                  fontSize: 13), textAlign: TextAlign.center,),
+            ),
+            TextFormField(
+              initialValue: 'Nomor Induk Kependudukan',
+              maxLength: 16,
+              decoration: InputDecoration(
+                fillColor: Colors.grey[300],
+                icon: Icon(Icons.badge),
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: kPrimaryColor)
+                )
+              ),
+            ),
+            TextFormField(
+              initialValue: 'Nomor Kartu Obat',
+              decoration: InputDecoration(
+                  fillColor: Colors.grey[300],
+                  icon: Icon(Icons.badge),
+                  enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: kPrimaryColor)
+                  )
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 2),
+              child: Text('Klik di sini untuk daftar pasien baru', style: kPustakaBlackRegular.copyWith(
+                  fontSize: 13), textAlign: TextAlign.center,),
+            ),
+            TextFormField(
+              initialValue: 'Nama',
+              decoration: InputDecoration(
+                  fillColor: Colors.grey[300],
+                  icon: Icon(Icons.person_rounded),
+                  enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: kPrimaryColor)
+                  )
+              ),
+            ),
+            TextFormField(
+              initialValue: 'Tanggal Lahir',
+              decoration: InputDecoration(
+                  fillColor: Colors.grey[300],
+                  icon: Icon(Icons.today_rounded),
+                  enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: kPrimaryColor)
+                  )
+              ),
+            ),
+            TextFormField(
+              initialValue: 'Alamat',
+              decoration: InputDecoration(
+                  fillColor: Colors.grey[300],
+                  icon: Icon(Icons.location_city_rounded),
+                  enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: kPrimaryColor)
+                  )
+              ),
+            ),
+          ],
+        ),
+      )
+    );
   }
 }
